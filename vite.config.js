312ef6b9b6b7c51bc8d116f6e76edaea88102e16
Mauricio Ojeda <jmojedap@gmail.com>
2026-03-27 10:05:36 -0500

    Configure GitHub Actions for deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/labasuraatiempo/', // Base URL of the GitHub Pages repository
   plugins: [
     vue(),
     vueDevTools(),
